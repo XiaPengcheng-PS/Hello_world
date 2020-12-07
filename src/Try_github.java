c72726894465bb304d866514bacce3f39d709603
XiaPengcheng-PS <2835162281@qq.com>
2020-12-07 15:58:01 +0800

    Test

---
+++ src/Try_github.java
@@ -0,0 +1,6 @@
+public class Try_github {
+    public static void main(String[] args){
+        System.out.println("Hi,GitHub\n I am XPC!");
+        System.out.println("Welcome to my world~");
+    }
+}
